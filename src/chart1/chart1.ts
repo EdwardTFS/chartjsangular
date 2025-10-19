ffa6fb7eaf4f60ee8c131a46a10f13c1ee14ac67
Edward Görlich <goerlich_e@tfs.pl>
2025-10-19 19:10:29 +0000

    charts

--- src/chart1/chart1.ts
+++ src/chart1/chart1.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart1',
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './chart1.html',
   styleUrl: './chart1.scss'
 })
 export class Chart1 {
+  readonly barChartData = {
+    datasets: [{
+      data: [{x: 'Sales', y: 20}, {x: 'Revenue', y: 10}]
+    }]
+  };
+  readonly barChartOptions = {};
+  readonly barChartType = 'bar';
 
 }
--- src/chart2/chart2.ts
+++ src/chart2/chart2.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart2',
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './chart2.html',
   styleUrl: './chart2.scss'
 })
 export class Chart2 {
-
+  readonly barChartData = {
+    datasets: [{
+      data: [{x: 'Sales', y: 20}, {x: 'Revenue', y: 10}]
+    }]
+  };
+  readonly barChartOptions = {};
+  readonly barChartType = 'bar';
 }
