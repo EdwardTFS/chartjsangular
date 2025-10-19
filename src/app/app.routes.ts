4d4d3dbb4d5d0074f992b1b895aced8ed94b2de1
Edward Görlich <goerlich_e@tfs.pl>
2025-10-19 18:46:06 +0000

    chart comopnents and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Chart1 } from '../chart1/chart1';
+import { Chart2 } from '../chart2/chart2';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: 'chart1', component: Chart1 },
+      { path: 'chart2', component: Chart2 },
+];
---
+++ src/chart2/chart2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart2',
+  imports: [],
+  templateUrl: './chart2.html',
+  styleUrl: './chart2.scss'
+})
+export class Chart2 {
+
+}
